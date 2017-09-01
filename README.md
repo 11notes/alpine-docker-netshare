94dba5cbc1c5e9ac7d420cba09366e5c05bdad47
11notes <github@11notes.ch>
2017-09-01 07:51:00 +0200

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # alpine-docker-netshare
+
+Install script to add docker-volume-netshare nfs plugin version 3 to default boot of alpine linux.
+Can also be used to add other plugins to boot (EFS, CIFS, NFSv4).
+
+## result after reboot
+```shell
+PID root       0:00 /usr/bin/docker-volume-netshare nfs -v 3
+```
+
+## build with
+
+* [Alpine Linux](https://alpinelinux.org/) - Alpine Linux OpenRC
+* [docker-volume-netshare](https://github.com/ContainX/docker-volume-netshare) - external filesystem plugin for docker volumes
+
+## tips
+
+* rc-update del docker-volume-nfs //remove script from default
---
+++ alpine-docker-netshare-nfs.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# install  nfs-utils
+apk update
+apk add nfs-utils \
+        ca-certificates \
+        wget
+rc-update add nfsmount
+rc-update add netmount
+update-ca-certificates
+
+# download docker-volume-netshare plugin and add to system startup
+wget -O /usr/bin/docker-volume-netshare \
+    https://github.com/ContainX/docker-volume-netshare/releases/download/v0.34/docker-volume-netshare_0.34_linux_amd64-bin \
+    && chmod +x /usr/bin/docker-volume-netshare
+
+# create OpenRC upstart script, set nfs version 3, and add to default
+# could be changed for CIFS, EFS, etc
+echo -e "#!/sbin/openrc-run\n\
+name=\"docker-volume-netshare nfs\"\n\
+command=\"/usr/bin/docker-volume-netshare\"\n\
+command_args=\"nfs -v 3\"
+pidfile=\"/var/run/docker-volume-netshare-nfs.pid\"\n\
+command_background=\"yes\"\n\
+depend(){\n\
+    need net\n\
+    need nfsmount\n\
+    need netmount\n\
+}\n" \
+    > /etc/init.d/docker-volume-nfs \
+    && chmod +x /etc/init.d/docker-volume-nfs \
+    && rc-update add docker-volume-nfs
